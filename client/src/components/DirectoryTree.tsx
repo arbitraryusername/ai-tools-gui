0fbb9661babf813ee9b038702f1262021e6fed7e
Craig W Pitcher <pitcher.craig@gmail.com>
2024-12-31 20:34:24 -0800

    PROMPT: in the DirectoryTree React component, the file names are not horizontally aligned with the checkboxes and the icons. the file names are lower. seems like the file names need to be a couple pixels higher so the bottom of the text is aligned with the bottom of the checkboxes and icons. fix this styling issue.

--- client/src/components/DirectoryTree.tsx
+++ client/src/components/DirectoryTree.tsx
@@ -129,6 +129,10 @@ const DirectoryTree: React.FC<DirectoryTreeProps> = ({ files, onCheckedChange })
           parentOpen: <FolderOpenIcon color="primary" />,
           leaf: <FileIcon color="primary" />,
         }}
+        labels={{
+          leaf: { style: { paddingTop: '2px' } },
+          parent: { style: { paddingTop: '2px' } }
+        }}
       />
     </Box>
   );
